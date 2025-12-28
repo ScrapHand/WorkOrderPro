47000ce38c9aebec35642f7911a910a456a220b7
ScrapHand <xeondivine@gmail.com>
2025-12-28 14:20:51 +0000

    fix(ui): remove syntax error breaking build in work order details

--- apps/legacy-web/src/app/[tenantSlug]/work-orders/[id]/page.tsx
+++ apps/legacy-web/src/app/[tenantSlug]/work-orders/[id]/page.tsx
@@ -472,7 +472,7 @@ export default function WorkOrderDetailPage({ params }: { params: Promise<{ tena
                     </div>
                 </div>
             )}
-            )}
+
 
             {/* Delete Confirmation Modal */}
             {isDeleteModalOpen && (
